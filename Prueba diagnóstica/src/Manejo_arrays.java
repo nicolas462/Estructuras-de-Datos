4d0524b79ceddee05517401ce8d3422d257a6529
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2018-08-12 15:04:11 -0500

    Punto 2

--- Prueba diagnóstica/src/Manejo_arrays.java
+++ Prueba diagnóstica/src/Manejo_arrays.java
@@ -30,6 +30,39 @@ public class Manejo_arrays {
 		return arrayInt;
 	}
 	
+	/**
+	 * Imprime arreglo.
+	 * @param array
+	 */
+	private static void print (int [] array)
+	{
+		for (int i = 0; i < array.length; i++)
+			System.out.print(array[i] + " ");
+	}
+	
+	/**
+	 * Bubblesort
+	 * @param array = Arreglo de enteros desordenado.
+	 */
+	public static void bubbleSort (int [] array) 
+	{
+		boolean swapped; //Se crea un booleano que dirá si hubo "swap" o no.
+		int swap; //Se crea una variable auxiliar para realizar el cambio dentro del vector.
+		for (int i =0; i< array.length -1; i++) 
+		{
+			swapped = false; //Antes de que comience el ciclo, el booleano quedará en Falso.
+			for (int j= 0; j< array.length-1; j++) 
+				if (array[j] > array[j+1]) 
+				{ //Si se cumple la condición se hace "swap".
+					swap = array [j];
+					array [j] = array [j+1];
+					array [j+1] = swap;
+					swapped = true; //Si no se entra en esta condición, el booleano quedará con el valor de antes (F).
+				}
+			if (!swapped) //Si el booleano sigue en falso, se rompe el ciclo.
+				break;	
+		}
+	}
 	
 	/**
 	 * 
@@ -48,6 +81,24 @@ public class Manejo_arrays {
 			for(int i = 0; i< arreglo.length; i++)
 				if(arreglo[i] == x)						//Si el número ingresado antes es igual, se imprime su índice.
 					System.out.print(i + " ");		
+		}
+		catch (Exception ex) {}
+	}
+	
+	/**
+	 * 
+	 */
+	public void punto2()
+	{
+		try
+		{
+			System.out.println("Ingrese la primera serie de números separados por espacio: ");
+			String line1 = br.readLine();
+			System.out.println("Ingrese la segunda serie de números: ");
+			String line2 = br.readLine();
+			int [] array = convertir(line1 + " " + line2);
+			bubbleSort(array);
+			print(array);
 		}
 		catch (Exception ex) {}
 	}
